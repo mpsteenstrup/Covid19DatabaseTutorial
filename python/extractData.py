77d7491026ddca9e664257311737a71a6ac0d5df
mpsteenstrup <mpsteenstrup@gmail.com>
2021-11-20 19:59:20 +0100

    extract covid data

---
+++ python/extractData.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+
+df = pd.read_csv(url, error_bad_lines=False)
+
+country = df.loc[df['Country/Region']=="Denmark"].index
+country = country[len(country)-1]
+
+data = df.to_numpy()
+nyeSmittede = np.diff(data[country,-40:])
+dage = np.array(range(0,len(nyeSmittede)))
+
+
+df = pd.DataFrame({"dage" : dage, "nye smittede" : nyeSmittede})
+df.to_csv("data.csv", index=False)
--- python/france.py
+++ python/france.py
@@ -19,7 +19,7 @@ url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_cov
 
 df = pd.read_csv(url, error_bad_lines=False)
 
-#print(df.head(100))
+#print(df.head(10))
 country = "France"
 country = df.loc[df['Country/Region']==country].index
 country = country[len(country)-1]
@@ -29,7 +29,7 @@ head = df.columns.to_numpy()
 #datoen = 475
 x = head[datoen:]
 y = data[country,datoen:].astype(int)
-#y = movingaverage(y,4)
+y = movingaverage(y,7)
 
 
 y = np.diff(y,n=1)*7/65e6*1e5
@@ -47,12 +47,14 @@ popt, pcov = curve_fit(lin,dage,y,[1.3,0.5])
 plt.plot_date(x[len(x)-len(y):],y)
 plt.plot(x[len(x)-len(y):],lin(dage,*popt),'r-')
 
+plt.xticks(np.arange(0, len(x)+1, 4))
+
 print(x[0])
 print('datoen {} er der  et incidenstal på {:.2f}'.format(x[-1],y[-1]))
 print('datoen {} er der det forventede incidenstal på {:.2f}'.format(x[-1],lin(len(x),*popt)))
 print('om en uge forventes den at være incidenstal på {:.2f}'.format(lin(len(x)+10,*popt)))
 
 
-plt.xticks(rotation=-90)
+plt.xticks(rotation=45)
 plt.grid()
 plt.show()
